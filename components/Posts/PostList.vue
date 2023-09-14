001cc239e35e370ab2936f90435ed1dcfed12d12
Austin Engelbrecht <45085255+austinengelbrecht@users.noreply.github.com>
2023-09-13 21:34:20 -0500

    Prep for backend data

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="featured-posts">
-      <PostPreview id="1" thumbnail="https://www.afterschoolafrica.com/wp-content/uploads/2020/03/Technology-industry-720x430-1.jpg" title="Hello There" previewText="My first post"/>
-      <PostPreview id="2" thumbnail="https://www.afterschoolafrica.com/wp-content/uploads/2020/03/Technology-industry-720x430-1.jpg" title="Hello There" previewText="My first post"/>
-      <PostPreview id="3" thumbnail="https://www.afterschoolafrica.com/wp-content/uploads/2020/03/Technology-industry-720x430-1.jpg" title="Hello There" previewText="My first post"/>
+      <PostPreview id="1" :is-admin="isAdmin" thumbnail="https://www.afterschoolafrica.com/wp-content/uploads/2020/03/Technology-industry-720x430-1.jpg" title="Hello There" previewText="My first post"/>
+      <PostPreview id="2" :is-admin="isAdmin" thumbnail="https://www.afterschoolafrica.com/wp-content/uploads/2020/03/Technology-industry-720x430-1.jpg" title="Hello There" previewText="My first post"/>
+      <PostPreview id="3" :is-admin="isAdmin" thumbnail="https://www.afterschoolafrica.com/wp-content/uploads/2020/03/Technology-industry-720x430-1.jpg" title="Hello There" previewText="My first post"/>
     </section>
 </template>
 
@@ -10,7 +10,13 @@
 import PostPreview from '@/components/Posts/PostPreview'
 
 export default {
-  components: { PostPreview }
+  components: { PostPreview },
+  props: { 
+    isAdmin: {
+      type: Boolean,
+      required: false
+    }
+  }
 }
 </script>
 
--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -27,3 +27,15 @@ data() {
 }
 
 </script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
